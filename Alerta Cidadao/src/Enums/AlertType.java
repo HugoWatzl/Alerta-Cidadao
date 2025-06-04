76488e34e7b06147b0d185816fbce7dec5cb4145
EduardoJacobPontes <162485552+EduardoJacobPontes@users.noreply.github.com>
2025-06-03 23:58:48 -0300

    Add files via upload

---
+++ Alerta Cidadao/src/Enums/AlertType.java
@@ -0,0 +1,12 @@
+package Enums;
+
+
+public enum AlertType {
+    TIROTEIO,
+    ARRASTAO,
+    OPERACAO_POLICIAL,
+    ASSALTO,
+    FURTO,
+    OUTRO,
+
+}
---
+++ Alerta Cidadao/src/model/Usuario.java
@@ -0,0 +1,34 @@
+package model;
+
+
+// import com.seuprojeto.alertacidadao.enums.TipoReacao;
+// import com.seuprojeto.alertacidadao.interfaces.I_Reagivel; // Pode ser necessário
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Usuario extends Pessoa {
+    private List<Alerta> alertasCriados;
+    private List<Comentario> comentarios;
+
+    public Usuario(int id, String nome, String email, String senha) {
+        super(id, nome, email, senha);
+        this.alertasCriados = new ArrayList<>();
+        this.comentarios = new ArrayList<>();
+    }
+
+    public void adicionarAlerta(Alerta alerta) {
+        this.alertasCriados.add(alerta);
+    }
+
+    public void adicionarComentario(Comentario comentario) {
+        this.comentarios.add(comentario);
+    }
+
+
+
+    // Você também pode ter métodos para o usuário realizar votação ou reações,
+    // que chamariam métodos nas instâncias de Alerta ou Comentario.
+    // Ex: public void votar(Alerta alerta, boolean aindaAcontecendo) { alerta.receberVoto(aindaAcontecendo); }
+    // Ex: public void reagir(I_Reagivel item, TipoReacao tipo) { item.receberReacao(new Reacao(this, tipo)); }
+}
