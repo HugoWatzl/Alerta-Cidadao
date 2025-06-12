d61454765b1e16219ec17b6a6f19118b09bfa79f
Hugo Watzl <143032400+HugoWatzl@users.noreply.github.com>
2025-06-11 22:55:00 -0300

    Update Usuario.java

--- src/main/java/Alerta_Cidadao/model/Usuario.java
+++ src/main/java/Alerta_Cidadao/model/Usuario.java
@@ -7,21 +7,21 @@ public class Usuario extends Pessoa {
     private List<Alerta> alertasCriados;
     private List<Comentario> comentarios;
 
-    // Construtor padrão corrigido
+    
     public Usuario() {
-        super(0, null, null, null); // Chama o construtor de Pessoa com valores padrão
+        super(0, null, null, null); 
         this.alertasCriados = new ArrayList<>();
         this.comentarios = new ArrayList<>();
     }
 
-    // Adicione um construtor com parâmetros
+    
     public Usuario(int id, String nome, String email, String senha) {
         super(id, nome, email, senha);
         this.alertasCriados = new ArrayList<>();
         this.comentarios = new ArrayList<>();
     }
 
-    // Métodos da classe
+    
     public void adicionarAlerta(Alerta alerta) {
         this.alertasCriados.add(alerta);
     }
